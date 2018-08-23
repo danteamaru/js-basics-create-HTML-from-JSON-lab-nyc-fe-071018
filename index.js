d77a8e812eeabb058c16468a8f82bbcc96f458d4
Dante Doig-Acuna <doig.acuna@gmail.com>
2018-08-23 23:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentloaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+  const titanicButton = document.getElementByID('Titanic')
+  const terminatorButton = document.getElementByID('Terminator 2')
+  titanicButton.addEventListener('click', (event) => {
+   //do something here
+  })
+   titanicButton.addEventListener('click', (event) => {
+   //do something here
+  
 });
